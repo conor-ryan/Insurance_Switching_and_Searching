e58141e7f608b265fcd72e135fd78b2aa25b4d63
Conor Ryan <cbr52@cornell.edu>
2019-04-22 10:03:05 -0500

    Spec Adjust

--- DSC_Estimation/Main.jl
+++ DSC_Estimation/Main.jl
@@ -59,109 +59,109 @@ rundate = Dates.today()
 
 
 #### Basic Switching Cost Specifications ####
-println("###############################")
-println("Basic Specification 1")
-filename = "basic_spec1_$rundate"
+# println("###############################")
+# println("Basic Specification 1")
+# filename = "basic_spec1_$rundate"
+# # df_est = df[(df[:hasi].==1).&(df[:dcat].==1),:]
+# basic_out_1 = MainSpec(df,filename,
+#     haltonDim = 1,
+#     spec_per = spec_per,
+#     spec_prd = spec_prd,
+#     spec_ch = spec_ch,
+#     spec_ch_last = spec_ch_last,
+#     spec_prodchr = [:padj,:iplan,:inet,:iiss],
+#     spec_prodchr_0= Vector{Symbol}(undef,0),
+#     spec_inertchr=Vector{Symbol}(undef,0),
+#     spec_demR=Vector{Symbol}(undef,0),
+#     spec_fixInt=Vector{Symbol}(undef,0),
+#     spec_fixEff=[:metal,:netname],
+#     spec_wgt= spec_wgt,
+#     method="ga",
+#     ga_itr=20)
+#
+# println("###############################")
+# println("Basic Specification 2")
+# filename = "basic_spec2_$rundate"
+# basic_out_2 = MainSpec(df,filename,
+#     haltonDim = 1,
+#     spec_per = spec_per,
+#     spec_prd = spec_prd,
+#     spec_ch = spec_ch,
+#     spec_ch_last = spec_ch_last,
+#     spec_prodchr = [:padj,:iplan,:inet,:iiss],
+#     spec_prodchr_0= Vector{Symbol}(undef,0),
+#     spec_inertchr=Vector{Symbol}(undef,0),
+#     spec_demR=Vector{Symbol}(undef,0),
+#     spec_fixInt=Vector{Symbol}(undef,0),
+#     spec_fixEff=[:product],
+#     spec_wgt= spec_wgt,
+#     method="ga",
+#     ga_itr=20)
+#
+#
+# println("###############################")
+# println("Basic Specification 3")
+# filename = "basic_spec3_$rundate"
+# basic_out_3 = MainSpec(df,filename,
+#     haltonDim = 1,
+#     spec_per = spec_per,
+#     spec_prd = spec_prd,
+#     spec_ch = spec_ch,
+#     spec_ch_last = spec_ch_last,
+#     spec_prodchr = [:padj,:iplan,:inet,:iiss],
+#     spec_prodchr_0= Vector{Symbol}(undef,0),
+#     spec_inertchr=Vector{Symbol}(undef,0),
+#     spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
+#     spec_fixInt=Vector{Symbol}(undef,0),
+#     spec_fixEff=[:metal,:netname],
+#     spec_wgt= spec_wgt,
+#     method="ga",
+#     ga_itr=20)
+#
+# println("###############################")
+# println("Basic Specification 4")
+# filename = "basic_spec4_$rundate"
+# basic_out_4 = MainSpec(df,filename,
+#     haltonDim = 1,
+#     spec_per = spec_per,
+#     spec_prd = spec_prd,
+#     spec_ch = spec_ch,
+#     spec_ch_last = spec_ch_last,
+#     spec_prodchr = [:padj,:iplan,:inet,:iiss],
+#     spec_prodchr_0= Vector{Symbol}(undef,0),
+#     spec_inertchr=Vector{Symbol}(undef,0),
+#     spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
+#     spec_fixInt=Vector{Symbol}(undef,0),
+#     spec_fixEff=[:product],
+#     spec_wgt= spec_wgt,
+#     method="ga",
+#     ga_itr=20)
+#
+# #### Basic Searching/Switching Specifications ####
+#
+# println("###############################")
+# println("Basic Searching Specification Test")
+# filename = "basic_search_test_$rundate"
+# # x_start, out = basic_out_1
+# # x_start = vcat([5],rand(6)/10 .-0.05,x_start)
 # df_est = df[(df[:hasi].==1).&(df[:dcat].==1),:]
-basic_out_1 = MainSpec(df,filename,
-    haltonDim = 1,
-    spec_per = spec_per,
-    spec_prd = spec_prd,
-    spec_ch = spec_ch,
-    spec_ch_last = spec_ch_last,
-    spec_prodchr = [:padj,:iplan,:inet,:iiss],
-    spec_prodchr_0= Vector{Symbol}(undef,0),
-    spec_inertchr=Vector{Symbol}(undef,0),
-    spec_demR=Vector{Symbol}(undef,0),
-    spec_fixInt=Vector{Symbol}(undef,0),
-    spec_fixEff=[:metal,:netname],
-    spec_wgt= spec_wgt,
-    method="ga",
-    ga_itr=20)
-
-println("###############################")
-println("Basic Specification 2")
-filename = "basic_spec2_$rundate"
-basic_out_2 = MainSpec(df,filename,
-    haltonDim = 1,
-    spec_per = spec_per,
-    spec_prd = spec_prd,
-    spec_ch = spec_ch,
-    spec_ch_last = spec_ch_last,
-    spec_prodchr = [:padj,:iplan,:inet,:iiss],
-    spec_prodchr_0= Vector{Symbol}(undef,0),
-    spec_inertchr=Vector{Symbol}(undef,0),
-    spec_demR=Vector{Symbol}(undef,0),
-    spec_fixInt=Vector{Symbol}(undef,0),
-    spec_fixEff=[:product],
-    spec_wgt= spec_wgt,
-    method="ga",
-    ga_itr=20)
-
-
-println("###############################")
-println("Basic Specification 3")
-filename = "basic_spec3_$rundate"
-basic_out_3 = MainSpec(df,filename,
-    haltonDim = 1,
-    spec_per = spec_per,
-    spec_prd = spec_prd,
-    spec_ch = spec_ch,
-    spec_ch_last = spec_ch_last,
-    spec_prodchr = [:padj,:iplan,:inet,:iiss],
-    spec_prodchr_0= Vector{Symbol}(undef,0),
-    spec_inertchr=Vector{Symbol}(undef,0),
-    spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
-    spec_fixInt=Vector{Symbol}(undef,0),
-    spec_fixEff=[:metal,:netname],
-    spec_wgt= spec_wgt,
-    method="ga",
-    ga_itr=20)
-
-println("###############################")
-println("Basic Specification 4")
-filename = "basic_spec4_$rundate"
-basic_out_4 = MainSpec(df,filename,
-    haltonDim = 1,
-    spec_per = spec_per,
-    spec_prd = spec_prd,
-    spec_ch = spec_ch,
-    spec_ch_last = spec_ch_last,
-    spec_prodchr = [:padj,:iplan,:inet,:iiss],
-    spec_prodchr_0= Vector{Symbol}(undef,0),
-    spec_inertchr=Vector{Symbol}(undef,0),
-    spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
-    spec_fixInt=Vector{Symbol}(undef,0),
-    spec_fixEff=[:product],
-    spec_wgt= spec_wgt,
-    method="ga",
-    ga_itr=20)
-
-#### Basic Searching/Switching Specifications ####
-
-println("###############################")
-println("Basic Searching Specification Test")
-filename = "basic_search_test_$rundate"
-# x_start, out = basic_out_1
-# x_start = vcat([5],rand(6)/10 .-0.05,x_start)
-df_est = df[(df[:hasi].==1).&(df[:dcat].==1),:]
-bs_out_1 = MainSpec(df_est,filename,
-    haltonDim = 1,
-    spec_per = spec_per,
-    spec_prd = spec_prd,
-    spec_ch = spec_ch,
-    spec_ch_last = spec_ch_last,
-    spec_prodchr = [:padj,:iplan,:inet,:iiss],
-    spec_prodchr_0= Vector{Symbol}(undef,0),
-    # spec_inertchr= [:constant,:padj],
-    spec_inertchr= [:constant,:agefe_1,:agefe_2,:fam,:hassub,:dprem,:def_padj,
-                    :def_mtl_brz,:def_mtl_cat,:def_mtl_gld,
-                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_s73,
-                    :def_mtl_s87,:def_mtl_s94],
-    spec_fixInt=Vector{Symbol}(undef,0),
-    spec_fixEff=[:metal,:netname],
-    spec_wgt= spec_wgt,
-    method="ga")
+# bs_out_1 = MainSpec(df_est,filename,
+#     haltonDim = 1,
+#     spec_per = spec_per,
+#     spec_prd = spec_prd,
+#     spec_ch = spec_ch,
+#     spec_ch_last = spec_ch_last,
+#     spec_prodchr = [:padj,:iplan,:inet,:iiss],
+#     spec_prodchr_0= Vector{Symbol}(undef,0),
+#     # spec_inertchr= [:constant,:padj],
+#     spec_inertchr= [:constant,:agefe_1,:agefe_2,:fam,:hassub,:dprem,:def_padj,
+#                     :def_mtl_brz,:def_mtl_cat,:def_mtl_gld,
+#                     :def_mtl_hdp,:def_mtl_plt,:def_mtl_s73,
+#                     :def_mtl_s87,:def_mtl_s94],
+#     spec_fixInt=Vector{Symbol}(undef,0),
+#     spec_fixEff=[:metal,:netname],
+#     spec_wgt= spec_wgt,
+#     method="ga")
 
 println("###############################")
 println("Basic Searching Specification 1")
@@ -197,7 +197,7 @@ bs_out_2 = MainSpec(df,filename,
     spec_prodchr_0= Vector{Symbol}(undef,0),
     spec_inertchr= [:constant,:agefe_1,:agefe_2,:fam,:hassub,:dprem,:def_padj,
                     :def_mtl_brz,:def_mtl_cat,:def_mtl_gld,
-                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_sl73,
+                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_s73,
                     :def_mtl_s87,:def_mtl_s94],
     spec_demR=Vector{Symbol}(undef,0),
     spec_fixInt=Vector{Symbol}(undef,0),
@@ -221,7 +221,7 @@ bs_out_3 = MainSpec(df,filename,
     spec_prodchr_0= Vector{Symbol}(undef,0),
     spec_inertchr= [:constant,:agefe_1,:agefe_2,:fam,:hassub,:dprem,:def_padj,
                     :def_mtl_brz,:def_mtl_cat,:def_mtl_gld,
-                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_sl73,
+                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_s73,
                     :def_mtl_s87,:def_mtl_s94],
     spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
     spec_fixInt=Vector{Symbol}(undef,0),
@@ -244,7 +244,7 @@ bs_out_4 = MainSpec(df,filename,
     spec_prodchr_0= Vector{Symbol}(undef,0),
     spec_inertchr= [:constant,:agefe_1,:agefe_2,:fam,:hassub,:dprem,:def_padj,
                     :def_mtl_brz,:def_mtl_cat,:def_mtl_gld,
-                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_sl73,
+                    :def_mtl_hdp,:def_mtl_plt,:def_mtl_s73,
                     :def_mtl_s87,:def_mtl_s94],
     spec_fixInt=Vector{Symbol}(undef,0),
     spec_fixEff=[:product],
