e6329b74ba81f24a504b44cde147b0f333d0d39c
Conor Ryan <cbr52@cornell.edu>
2019-08-01 19:51:41 -0500

    Adj, test person definition

--- DSC_Estimation/Main.jl
+++ DSC_Estimation/Main.jl
@@ -23,11 +23,11 @@ println("Code Loaded")
 
 # Load the Data
 include("load.jl")
-df_LA = df[df[:gra].==16,:]
-# df_LA = df[df[:hasi].==1.0,:]
-# df_LA = df
-### Add Anthem Fixed Effect
-df_LA[:issfe_1] = Int.(df_LA[:issuername].=="Anthem")
+# df_LA = df[df[:gra].==16,:]
+# # df_LA = df[df[:hasi].==1.0,:]
+df_LA = df
+# ### Add Anthem Fixed Effect
+# df_LA[:issfe_1] = Int.(df_LA[:issuername].=="Anthem")
 
 println("Data Loaded")
 
@@ -41,9 +41,9 @@ mixed_draws = 500
 
 ### interact default plans and "active" choice with preference parameters
 
-rundate = Dates.today()
-file = "$(homedir())/Documents/Research/CovCAInertia/Output/Estimation_Results/ML_spec6_2019-05-25.jld2"
-@load file p_est spec_Dict fval
+# rundate = Dates.today()
+# file = "$(homedir())/Documents/Research/CovCAInertia/Output/Estimation_Results/ML_spec6_2019-05-25.jld2"
+# @load file p_est spec_Dict fval
 
 # println("###############################")
 # println("Specification 0")
@@ -67,50 +67,74 @@ file = "$(homedir())/Documents/Research/CovCAInertia/Output/Estimation_Results/M
 #
 # x_start = mx_out_1[1]
 
-println("###############################")
-println("Specification 0")
-### Only plan-level switching cost, no Inertia
-filename = "Spec0b_$rundate"
-mx_out_1 = MainSpec(df_LA,filename,
-    haltonDim = spec_Dict["haltonDim"],
-    spec_per =[:household],
-    spec_prd = spec_Dict["prd"],
-    spec_ch = spec_Dict["ch"],
-    spec_ch_last = spec_Dict["ch_last"],
-    spec_prodchr = spec_Dict["prodchr"],
-    spec_prodchr_0= spec_Dict["prodchr_0"],
-    spec_inertchr = spec_Dict["inertchr"],
-    spec_demR=spec_Dict["demR"],
-    spec_prodInt=spec_Dict["prodInt"],
-    spec_fixInt=Vector{Symbol}(undef,0),
-    spec_fixEff=spec_Dict["fixEff"],
-    spec_wgt=spec_Dict["wgt"],
-    method="ga",ga_itr = 50,x_start = p_est)
-
-#
 # println("###############################")
-# println("Specification 1")
+# println("Specification 0")
 # ### Only plan-level switching cost, no Inertia
-# filename = "Spec1_$rundate"
+# filename = "Spec0b_$rundate"
 # mx_out_1 = MainSpec(df_LA,filename,
-#     haltonDim = mixed_draws,
-#     spec_per = spec_per,
-#     spec_prd = spec_prd,
-#     spec_ch = spec_ch,
-#     spec_ch_last = spec_ch_last,
-#     spec_prodchr = [:padj,:iplan,
-#     :issfe_1, :issfe_2, :issfe_3, :issfe_4,
-#     :issfe_6, :issfe_7, # Leave Out LA Care
-#     :netfe_2, :netfe_3, :netfe_4, :netfe_6,
-#     :netfe_8, :netfe_9, :netfe_10, :netfe_12],
-#     spec_prodchr_0= [:issfe_1, :issfe_2, :issfe_3, :issfe_4],
-#     spec_inertchr= Vector{Symbol}(undef,0),
-#     spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
-#     spec_prodInt=[:padj,:iplan],
+#     haltonDim = spec_Dict["haltonDim"],
+#     spec_per =[:household],
+#     spec_prd = spec_Dict["prd"],
+#     spec_ch = spec_Dict["ch"],
+#     spec_ch_last = spec_Dict["ch_last"],
+#     spec_prodchr = spec_Dict["prodchr"],
+#     spec_prodchr_0= spec_Dict["prodchr_0"],
+#     spec_inertchr = spec_Dict["inertchr"],
+#     spec_demR=spec_Dict["demR"],
+#     spec_prodInt=spec_Dict["prodInt"],
 #     spec_fixInt=Vector{Symbol}(undef,0),
-#     spec_fixEff=[:metal],
-#     spec_wgt= spec_wgt,
-#     method="ga",ga_itr = 50,ll_start = true)
+#     spec_fixEff=spec_Dict["fixEff"],
+#     spec_wgt=spec_Dict["wgt"],
+#     method="ga",ga_itr = 50,x_start = p_est)
+println("###############################")
+println("Specification 1")
+### Only plan-level switching cost, no Inertia
+filename = "Spec1b_$rundate"
+mx_out_1 = MainSpec(df_LA,filename,
+    haltonDim = mixed_draws,
+    spec_per = [:hh_id],
+    spec_prd = spec_prd,
+    spec_ch = spec_ch,
+    spec_ch_last = spec_ch_last,
+    spec_prodchr = [:padj,:iplan,
+    :issfe_1, :issfe_2, :issfe_3, :issfe_4,
+    :issfe_6, :issfe_7, # Leave Out LA Care
+    :netfe_2, :netfe_3, :netfe_4, :netfe_6,
+    :netfe_8, :netfe_9, :netfe_10, :netfe_12],
+    spec_prodchr_0= [:issfe_1, :issfe_2, :issfe_3, :issfe_4],
+    spec_inertchr= Vector{Symbol}(undef,0),
+    spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
+    spec_prodInt=[:padj,:iplan],
+    spec_fixInt=Vector{Symbol}(undef,0),
+    spec_fixEff=[:metal],
+    spec_wgt= spec_wgt,
+    method="ga",ga_itr = 50,ll_start = true)
+#
+println("###############################")
+println("Specification 1")
+### Only plan-level switching cost, no Inertia
+filename = "Spec1_$rundate"
+mx_out_1 = MainSpec(df_LA,filename,
+    haltonDim = mixed_draws,
+    spec_per = spec_per,
+    spec_prd = spec_prd,
+    spec_ch = spec_ch,
+    spec_ch_last = spec_ch_last,
+    spec_prodchr = [:padj,:iplan,
+    :issfe_1, :issfe_2, :issfe_3, :issfe_4,
+    :issfe_6, :issfe_7, # Leave Out LA Care
+    :netfe_2, :netfe_3, :netfe_4, :netfe_6,
+    :netfe_8, :netfe_9, :netfe_10, :netfe_12],
+    spec_prodchr_0= [:issfe_1, :issfe_2, :issfe_3, :issfe_4],
+    spec_inertchr= Vector{Symbol}(undef,0),
+    spec_demR=[:agefe_1,:agefe_2,:fam,:hassub],
+    spec_prodInt=[:padj,:iplan],
+    spec_fixInt=Vector{Symbol}(undef,0),
+    spec_fixEff=[:metal],
+    spec_wgt= spec_wgt,
+    method="ga",ga_itr = 50,ll_start = true)
+
+
 #
 # println("###############################")
 # println("Specification 2")
