0b079c41fbbb907a5039443211db687b0879754c
Conor Ryan <cbr52@cornell.edu>
2019-10-22 10:28:24 -0500

    Take dprem out of load, replace with autodp

--- DSC_Estimation/load.jl
+++ DSC_Estimation/load.jl
@@ -8,7 +8,7 @@ df = CSV.read("$(homedir())/Documents/Research/CovCAInertia/Output/analysis_i2.c
 
 # No constant
 df[:constant] = ones(size(df, 1))
-df[:dprem][ismissing.(df[:dprem])] .= 0.0
+df[:autodp][ismissing.(df[:autodp])] .= 0.0
 for key in [:lag_disc,:def_padj,:def_mtl_brz,:def_mtl_cat,:def_mtl_gld,:def_mtl_plt,:def_mtl_s73,:def_mtl_s87,:def_mtl_s94]
     df[key][df[:hasi].==0] .= 0.0
 end
