d546edbe5d49aabdc25909b85302506fffc1aeae
Conor Ryan <ryan0463@umn.edu>
2021-08-24 14:27:56 -0400

    Third half of the runs.

--- DSC_Estimation/load.jl
+++ DSC_Estimation/load.jl
@@ -9,6 +9,8 @@ df = CSV.read("$(homedir())/Documents/Research/CovCAInertia/Output/analysis_i2.c
 # No constant
 df[:constant] = ones(size(df, 1))
 df[:autodp][ismissing.(df[:autodp])] .= 0.0
+df[:choiceset_exceed_thresh_95][ismissing.(df[:choiceset_exceed_thresh_95])] .= 0.0
+df[:choiceset_exceed_thresh_98][ismissing.(df[:choiceset_exceed_thresh_98])] .= 0.0
 for key in [:def_mtl_brz,:def_mtl_cat,:def_mtl_gld,:def_mtl_plt,:def_mtl_slv,:def_mtl_hdp]
     df[key][df[:autoelig].==0] .= 0.0
 end
