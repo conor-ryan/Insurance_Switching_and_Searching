861f682b81649e739ed62b9ba9073b18f6a8ef36
Conor Ryan <ryan0463@umn.edu>
2021-09-08 14:09:09 -0400

    Test Run

--- DSC_Estimation/load.jl
+++ DSC_Estimation/load.jl
@@ -4,7 +4,7 @@ using CSV
 #################### Loading and Cleaning Data #########################
 ########################################################################
 # Load the data
-df = CSV.read("$(homedir())/Documents/Research/CovCAInertia/Output/analysis_i2.csv";copycols=true)
+df = CSV.read("$home/Output/analysis_i2.csv";copycols=true)
 
 # No constant
 df[:constant] = ones(size(df, 1))
@@ -15,7 +15,7 @@ for key in [:def_mtl_brz,:def_mtl_cat,:def_mtl_gld,:def_mtl_plt,:def_mtl_slv,:de
     df[key][df[:autoelig].==0] .= 0.0
 end
 
-df_active = CSV.read("$(homedir())/Documents/Research/CovCAInertia/Output/analysis_i2_active.csv";copycols=true)
+df_active = CSV.read("$home/Output/analysis_i2_active.csv";copycols=true)
 
 # No constant
 df_active[:constant] = ones(size(df_active, 1))
