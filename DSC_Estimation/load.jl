4c967a123f2db60e8d3f4c6b96c81c6d723e9971
Conor Ryan <ryan0463@umn.edu>
2021-09-29 11:56:55 -0400

    Spec adjust. Also the changes in the active prediction file. Mostly scratch work.

--- DSC_Estimation/load.jl
+++ DSC_Estimation/load.jl
@@ -11,6 +11,10 @@ df[:constant] = ones(size(df, 1))
 df[:autodp][ismissing.(df[:autodp])] .= 0.0
 df[:choiceset_exceed_thresh_95][ismissing.(df[:choiceset_exceed_thresh_95])] .= 0.0
 df[:choiceset_exceed_thresh_98][ismissing.(df[:choiceset_exceed_thresh_98])] .= 0.0
+df[:choiceset_below_thresh_05][ismissing.(df[:choiceset_below_thresh_05])] .= 0.0
+df[:choiceset_below_thresh_02][ismissing.(df[:choiceset_below_thresh_02])] .= 0.0
+
+
 for key in [:def_mtl_brz,:def_mtl_cat,:def_mtl_gld,:def_mtl_plt,:def_mtl_slv,:def_mtl_hdp]
     df[key][df[:autoelig].==0] .= 0.0
 end
--- Plots/ActivePrediction.R
+++ Plots/ActivePrediction.R
@@ -101,3 +101,48 @@ ggplot(df) + aes(x=active_lab,y=mean,ymin=min,ymax=max) +
     axis.title=element_text(size=12),
     axis.text = element_text(size=12))
 dev.off()
+
+#### Inattention and Switching Costs ####
+#### Spec 3 ####
+rundate = "2021-08-27"
+spec = "Spec3_"
+df_full = as.data.table(read.csv(paste("Output/Estimation_Results/active_",spec,rundate,".csv",sep="")))
+df_full = df_full[returning==1,]
+
+#### Get Demographic Info ####
+est_data = as.data.table(read.csv("Output/analysis_i2.csv"))
+demos = unique(est_data[,c("year","hh_id","agefe_1","agefe_2","fam","hassub")])
+
+wtp_3 = as.data.table(read.csv(paste("Output/Estimation_Results/wtp_Spec3_",rundate,".csv",sep="")))
+wtp_3[,agefe_1:=rep(c(0,1,0),4)]
+wtp_3[,agefe_2:=rep(c(0,0,1),4)]
+wtp_3[,fam:=rep(c(rep(0,3),rep(1,3)),2)]
+wtp_3[,hassub:=c(rep(1,6),rep(0,6))]
+wtp_3[,switching_cost_3:=x1+x2]
+wtp_3[,c("x1","x2"):=NULL]
+demos = merge(demos,wtp_3,by=c("agefe_1","agefe_2","fam","hassub"))
+
+## Run this puppy. 
+# wtp_4 = as.data.table(read.csv(paste("Output/Estimation_Results/wtp_Spec4_",rundate,".csv",sep="")))
+# wtp_4[,agefe_1:=rep(c(0,1,0),4)]
+# wtp_4[,agefe_2:=rep(c(0,0,1),4)]
+# wtp_4[,fam:=rep(c(rep(0,3),rep(1,3)),2)]
+# wtp_4[,hassub:=c(rep(1,6),rep(0,6))]
+# wtp_4[,switching_cost_4:=x1+x2]
+# wtp_4[,c("x1","x2"):=NULL]
+# demos = merge(demos,wtp_4,by=c("agefe_1","agefe_2","fam","hassub"))
+
+demos[,alpha_3:=-2.42+agefe_1*0.94+agefe_2*1.35+fam*0.74+hassub*-0.30]
+demos[,alpha_4:=-2.29+agefe_1*0.91+agefe_2*1.30+fam*0.69+hassub*-0.29]
+demos[,beta_3:=(1.74+1.14) + (-0.03+-0.07)*agefe_1 + (0.19+-0.22)*agefe_2 + (0.02+-0.12)*fam + (0.03+-0.46)*hassub]
+demos[,beta_4:=(1.70+1.07) + (0.14+-0.05)*agefe_1 + (0.44+-0.25)*agefe_2 + (-0.04+-0.13)*fam + (0.02+-0.52)*hassub]
+
+demos[,sc_test_3:=-100*(beta_3/alpha_3)]
+demos[,switching_cost_4:=-100*beta_4/alpha_4]
+
+df_full = merge(df_full,demos,by.x=c("Person","Year"),by.y=c("hh_id","year"),all.x=TRUE)
+
+table = df_full[,list(active_pred=mean(active_pred),active_obs=mean(active_obs),sc_3=mean(switching_cost_3),sc_4=mean(switching_cost_4)),by=c("agefe_1","agefe_2","fam","hassub")]
+
+
+ggplot(table) + geom_point(aes(x=sc_3,y=active_pred),color="blue") + geom_smooth(aes(x=sc_3,y=active_pred),color="blue",se=FALSE,method="lm")
